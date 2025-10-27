715ee4a730ffa77ddb8cac84d6d08618cec8cb0f
raval21 <129948279+raval21@users.noreply.github.com>
2025-10-27 01:57:32 -0700

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,15 @@
+// Countdown Timer
+const eventDate = new Date("2025-08-27 00:00:00").getTime();
+
+setInterval(() => {
+  const now = new Date().getTime();
+  const distance = eventDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000*60*60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000*60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML =
+    `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}, 1000);
